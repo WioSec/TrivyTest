ec3fa23a6f3f04dd9248203abb00b5693455d1bf
WioSec <111039045+WioSec@users.noreply.github.com>
2022-08-25 16:08:41 +0400

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,274 @@
+bucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+                          bucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+                          bucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+                          bucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitivebucket = s3.Bucket(self, "bucket",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
+)
+
+s3deploy.BucketDeployment(self, "DeployWebsite",
+    access_control=s3.BucketAccessControl.PUBLIC_READ_WRITE     # Sensitive
