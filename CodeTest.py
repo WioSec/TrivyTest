4ed2f34d625f12fd0479ad797f3b4a2ef0fdd407
WioSec <111039045+WioSec@users.noreply.github.com>
2022-08-25 15:41:24 +0400

    Update CodeTest.py

--- CodeTest.py
+++ CodeTest.py
@@ -1,26 +1,41 @@
-int VerifyAdmin(char *password) {
-if (strcmp(password,"68af404b513073584c4b6f22b6c63e6b")) {
-
-printf("Incorrect Password!\n");
-return(0);
-}
-printf("Entering Diagnostic Mode...\n");
-return(1);
-}
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
 
 
-int VerifyAdmin(char *password) {
-if (strcmp(password, "Mew!")) {
-printf("Incorrect Password!\n");
-return(0)
-}
-printf("Entering Diagnostic Mode...\n");
-return(1);
-}
-public boolean VerifyAdmin(String password) {
-if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
-System.out.println("Entering Diagnostic Mode...");
-return true;
-}
-System.out.println("Incorrect Password!");
-return false;
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
+
+username = 'admin'
+password = 'admin' # Sensitive
+usernamePassword = 'user=admin&password=admin' # Sensitive
